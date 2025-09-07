d308f2c805004547e4c812d8eedc26ad0b06effa
iAmAbhishek2584 <abhishek.kotnala2584@gmail.com>
2025-09-07 13:13:47 +0530

    Linkedlist all operations implementation

--- src/datastructures/linkedlists/LinkedList.java
+++ src/datastructures/linkedlists/LinkedList.java
@@ -47,9 +47,146 @@ public class LinkedList {
         length++;
     }
 
-    // removing last item from linkedlist
-    public void removeAtLast() {
+    // prepend operation
+    public void prepend(int value) {
+        Node newNode = new Node(value);
 
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    // removing first item from linkedlist
+    public Node removeAtFirst() {
+        if (length == 0) {
+            return null;
+        }
+
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+
+        if (length == 0) {
+            tail = null;
+        }
+
+        return temp;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+
+        if (index == 0) return removeAtFirst();
+
+        if (index == length -1) return removeAtLast();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length --;
+        return temp;
+    }
+
+    // insert
+    public boolean insert(int index, int value) {
+
+        if (index < 0 || index > length) return false;
+
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+
+        if (index == length) {
+            append(value);
+            return true;
+        }
+
+        Node newNode = new Node(value);
+        Node temp = get(index-1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    // removing last item from linkedlist
+    public Node removeAtLast() {
+
+        if (length == 0) {
+            return null;
+        }
+        Node temp = head;
+        Node pre = head;
+
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+
+        tail = pre;
+        tail.next = null;
+        length--;
+
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
+
+    // reversing linkedlist
+    public void reverse() {
+
+        //flipping head and tail
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        Node after = temp.next;
+        Node before = null;
+
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+
+
+    }
+
+    // get element at an index
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        } else {
+            Node temp = head;
+
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+
+    // set element at an index
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+
+        if (temp!=null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
     }
 
 
--- src/datastructures/linkedlists/Tester.java
+++ src/datastructures/linkedlists/Tester.java
@@ -5,13 +5,15 @@ public class Tester {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(5);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-
         myLinkedList.append(6);
         myLinkedList.append(7);
         myLinkedList.append(8);
+
+        myLinkedList.printList();
+
+        System.out.println("Reversing the linked list");
+        myLinkedList.reverse();
+
         myLinkedList.printList();
     }
 }
